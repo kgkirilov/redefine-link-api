4dea34214d6e9851df4e6e2dbe51645fc104825e
K. G. Kirilov <kgkirilov7@gmail.com>
2019-05-29 16:28:09 +0300

    Initial commit

---
+++ src/main/java/link/redefine/api/rdfn/RdfnApplication.java
@@ -0,0 +1,13 @@
+package link.redefine.api.rdfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdfnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdfnApplication.class, args);
+	}
+
+}
